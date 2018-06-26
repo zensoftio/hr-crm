94db53a4295a294810eed669aa0132418125ae4d
erlanamanatov <erlanamanatov@gmail.com>
2018-06-26 15:15:03 +0600

    Project structure created. NavigationView items renamed

--- Android/ZensoftHRCRM/app/src/main/java/com/erkprog/zensofthrcrm/MainActivity.java
+++ Android/ZensoftHRCRM/app/src/main/java/com/erkprog/zensofthrcrm/MainActivity.java
@@ -1,8 +1,6 @@
 package com.erkprog.zensofthrcrm;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -22,15 +20,6 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -80,17 +69,15 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_requests) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_vacancies) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_candidates) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_interviews) {
 
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_statistics) {
 
         }
 
