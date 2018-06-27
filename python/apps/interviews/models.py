3515b22cea605b00ebf70ee4646a7e23c975b89d
quanta <quantagonista@gmail.com>
2018-06-27 13:06:34 +0600

    Dockerize + Interview Model + Config Edits

--- python/apps/interviews/models.py
+++ python/apps/interviews/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Interview(models.Model):
+    candidate = models.ForeignKey('Candidate', on_delete=models.PROTECT)
+    date = models.DateTimeField()
+    request = models.ForeignKey('Request', on_delete=models.PROTECT)
