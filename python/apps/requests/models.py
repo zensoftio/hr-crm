0dcb89da7701d5e951b3ffc7df2e04a7fa2d0db2
Emir <amanbekoff@gmail.com>
2018-06-27 13:10:25 +0600

    Add requests model

--- python/apps/requests/models.py
+++ python/apps/requests/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+from apps.departments.models import Position
+
+User = get_user_model()
+
+
+class Request(models.Model):
+    count = models.IntegerField()
+    created = models.DateTimeField(auto_now_add=True)
+    created_by = models.ForeignKey(User, on_delete=models.PROTECT)
+    modified = models.DateTimeField(auto_now=True)
+    position = models.ForeignKey(Position, on_delete=models.PROTECT)
+    status = models.IntegerField()
+
+    def __str__(self):
+        return self.position.name
