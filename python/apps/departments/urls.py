0ddd8d08df81ca9ed98d10aeab12ce56b7922bf2
Oma Dulatov <oma.dulatov@gmail.com>
2018-06-27 15:12:59 +0600

    add DepartmentSerializer class, and add urls for list of department, and add views for list of department.

--- python/apps/departments/urls.py
+++ python/apps/departments/urls.py
@@ -1,1 +1,8 @@
-urlpatterns = []
+from django.urls import path
+from apps.departments import views
+
+
+urlpatterns = [
+    path('', views.DepartmentList.as_view()),
+]
+
--- python/apps/departments/views.py
+++ python/apps/departments/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework import generics
+from apps.departments.models import Department
+from apps.departments.serializers import DepartamentSerializer
 
-# Create your views here.
+
+class DepartmentList(generics.ListCreateAPIView):
+    queryset = Department.objects.all()
+    serializer_class = DepartamentSerializer
