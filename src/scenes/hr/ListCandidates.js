a58e1e9ff9131c8535794ba32c3bb711b72621d8
Islambek <islambekakylbekuulu@gmail.com>
2018-06-28 10:35:40 +0600

    Created list of candidates table for HR

---
+++ src/scenes/hr/ListCandidates.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from '../general/Header';
+
+const ListCandidates = () => {
+    return (
+        <div>
+            <Header title="Список кандидатов"/>
+
+             {/*<ListOfCandidates/>*/}
+        </div>
+    )
+}
+
+export default ListCandidates;
+
