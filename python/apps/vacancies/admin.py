df04389c88b1c9d437ff543e88cdd8bd2b0b5f7e
Oma Dulatov <oma.dulatov@gmail.com>
2018-06-28 16:41:58 +0600

    fix admin.py on Vacancy

--- python/apps/vacancies/admin.py
+++ python/apps/vacancies/admin.py
@@ -1,12 +1,5 @@
 from django.contrib import admin
-
-from apps.departments.models import Position, Department
-from apps.requests.models import Request
 from apps.vacancies.models import Vacancy
 
-# Register your models here.
 
-admin.site.register(Department)
-admin.site.register(Position)
-admin.site.register(Request)
 admin.site.register(Vacancy)
