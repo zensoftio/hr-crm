1934c3121426b6f7d8aa45f68ed1b699c924ccbd
AlanMamphs <mamunovalisher@gmail.com>
2018-06-28 09:26:04 +0600

    Python. Edit Url.py. Added paths to publication, and vacancy views

--- python/apps/vacancies/urls.py
+++ python/apps/vacancies/urls.py
@@ -1,1 +1,12 @@
-urlpatterns = []
+from .views import VacancyList, VacancyDetail, PublicationList, PublicationDetail
+
+from django.urls import path
+
+urlpatterns = (
+    path('', VacancyList.as_view(), name='vacancy-list'),
+    path('<int:pk>', VacancyDetail.as_view(), name='vacancy-detail'),
+    path('', PublicationList.as_view(), name='publication-list'),
+    path('<int:pk>', PublicationDetail.as_view(), name='publication-detail')
+)
+
+
