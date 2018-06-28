159776d1d3a91eaa1fb3cf21458d69a281ade14e
Almaz <mositosi97@gmail.com>
2018-06-28 12:08:05 +0600

    Return test_func

--- python/apps/candidates/views.py
+++ python/apps/candidates/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from rest_framework import generics
+from django.http import HttpResponse
 
 from apps.candidates.models import Candidate
 from apps.candidates.serializers import CandidateSerializer
 
+def test_func(request):
+    return HttpResponse('Test')
 
 class CandidateList(generics.ListAPIView):
     queryset = Candidate.objects.all()
