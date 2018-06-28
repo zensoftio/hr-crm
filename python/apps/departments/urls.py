e86fd1cffa1679e76475c76d18bc17808a98ed12
Oma Dulatov <oma.dulatov@gmail.com>
2018-06-28 13:23:51 +0600

    Retrieve list of requirements request

--- python/apps/departments/urls.py
+++ python/apps/departments/urls.py
@@ -4,7 +4,10 @@ from apps.departments import views
 
 urlpatterns = [
     path('departments/', views.DepartmentCreateListView.as_view()),
-    path('departments/<int:pk>', views.RequirementCreateView.as_view()),
+    path('departments/<int:pk>/', views.DepartmentRetrieve.as_view()),
     path('positions/', views.PositionListCreateView.as_view()),
+    path('positions/<int:pk>/', views.PositionRetrieve.as_view()),
+    path('requirements/', views.RequirementCreateView.as_view()),
+    path('requirements/<int:pk>', views.RequirementRetrieve.as_view()),
 ]
 
