45dbb4caf49fc93f3c2ca2c15bfab78492c402d9
Daria Ree <ri_d@auca.kg>
2018-06-30 16:14:32 +0600

    Saves a send message to DB(tables - Message, Recipient) Getting a message from RabbitMQ, send a message to specified reciepients, with a specified template and saving the sent message to a database(tables - Message, Recipient)

--- NestJS/src/app.module.ts
+++ NestJS/src/app.module.ts
@@ -18,16 +18,7 @@ import { EventModule } from 'Events/event.module';
     MessageModule,
     RecepientModule,
     EventModule,
-    TypeOrmModule.forRoot({
-      "type": "postgres",
-      "host": "localhost",
-      "port": 5432,
-      "username": "root",
-      "password": "root",
-      "database": "test4",
-      "entities": ["src/**/**.entity{.ts,.js}"],
-      "synchronize": true
-  })
+    TypeOrmModule.forRoot()
   ],
   controllers: [AppController],
   providers: [AppService],
