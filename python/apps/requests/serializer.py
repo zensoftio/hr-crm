69e6b8938451efe769993954beea430f4e48e855
Emir Amanbekov <amanbekoff@gmail.com>
2018-06-30 14:19:53 +0600

    Add request creation * Add request detail serializer * Override create method * Add request create serializer

--- python/apps/requests/serializer.py
+++ python/apps/requests/serializer.py
@@ -1,8 +1,9 @@
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
 
+from apps.departments.serializers import AuxPositionSerializer, RequirementSerializer
 from apps.requests.models import Request
-from apps.departments.serializers import AuxPositionSerializer
+from apps.users.serializers import AuxUserSerializer
 
 User = get_user_model()
 
@@ -13,6 +14,22 @@ class RequestListSerializer(serializers.ModelSerializer):
     class Meta:
         model = Request
         fields = ('id', 'position', 'status', 'count', 'created')
+
+
+class RequestCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Request
+        exclude = []
+
+
+class RequestDetailSerializer(serializers.ModelSerializer):
+    created_by = AuxUserSerializer(read_only=True)
+    position = AuxPositionSerializer(read_only=True)
+    requirements = RequirementSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Request
+        fields = ('id', 'created_by', 'position', 'requirements', 'created', 'status', 'count', 'modified')
 
 
 class RequestInterviewSerializer(serializers.ModelSerializer):
