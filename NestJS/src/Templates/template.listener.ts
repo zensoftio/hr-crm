afa8138306a6982058e1a536931fe7a045e4cb31
Marat Tynarbekov <tynarbekov95@gmail.com>
2018-07-01 05:03:44 +0600

    NestJS Configure TemplateListener. Create,update by request from RabbitMq and send response

--- NestJS/src/Templates/template.listener.ts
+++ NestJS/src/Templates/template.listener.ts
@@ -2,20 +2,51 @@ import { Controller } from '@nestjs/common';
 import * as Amqp from "amqp-ts";
 import { TemplateService } from './template.service';
 
-var connection = new Amqp.Connection("amqp://localhost");
-var exchange = connection.declareExchange("test3", 'direct', { durable: false });
-var queue = connection.declareQueue('');
+const connection = new Amqp.Connection("amqp://localhost");
+const exchange = connection.declareExchange("exchangeForTemplate", 'direct', { durable: false });
+const queue = connection.declareQueue('template');
 
 @Controller('template')
 export class TemplateListener {
     constructor(private readonly templateService: TemplateService){
+      this.listenQueue();
+    }
+
+    public distributionTasks(task){
+      if (task.title == "CREATE")this.createTemplate(task);
+      else if(task.title == "UPDATE")this.updateTamplate(task);
+      else if(task.title == "DELETE")this.deleteTemplate(task);
+      else if(task.title == "FIND_ONE")this.findOneTemplate(task);
+      else if(task.title == "GET_ALL")this.findAllTemplate(task);
+    }
+
+    async updateTamplate(template){
+      this.templateService.update(template.id,template)
+      .then( (temp) => { this.sendMessage(temp) })
+      .catch( (err) => { this.sendMessage("Not Updated") })
+    }
+
+    async createTemplate(template){
+      this.templateService.create(template)
+      .then( (temp) => { this.sendMessage(temp) })
+      .catch( (err) => { this.sendMessage("Not Created") })
+    }
+
+    private async sendMessage(msg){
+      var sendQueue = connection.declareQueue("template2")
+      connection.completeConfiguration().then(() => {
+          var msg2 = new Amqp.Message(msg);
+          exchange.send(msg2);
+      });
+    }
+
+    private async listenQueue(){
       queue.bind(exchange, 'template');
       queue.activateConsumer((message) => {
         var msg = message.getContent()
-        var data = JSON.parse(msg)
-        console.log(msg)
-        templateService.create(data)
+        msg = JSON.parse(msg)
+        this.distributionTasks(msg);
         }, {noAck: true})
-
     }
+
 }
