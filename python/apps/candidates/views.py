4b9aa5949f8263f310d0a9677bf6e37621fcd054
quanta <quantagonista@gmail.com>
2018-06-30 12:10:26 +0600

    Add Candidate Update Endpoint

--- python/apps/candidates/views.py
+++ python/apps/candidates/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
 from rest_framework import generics
+from rest_framework.response import Response
 
+from apps.departments.models import Position
 from .models import Candidate
 from .serializers import AuxCandidateSerializer, CandidateDetailSerializer
 
@@ -17,3 +19,15 @@ class CandidateListView(generics.ListAPIView):
 class CandidateDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Candidate.objects.all()
     serializer_class = CandidateDetailSerializer
+
+    def update(self, request, *args, **kwargs):
+        position = request.data.get('position')
+        if position:
+            position = Position.objects.get(pk=position)
+            candidate = Candidate.objects.get(pk=kwargs.get('pk'))
+            candidate.position = position
+            candidate.save()
+            serializer = CandidateDetailSerializer(candidate)
+            return Response(serializer.data)
+        else:
+            return super().update(request, *args, **kwargs)
