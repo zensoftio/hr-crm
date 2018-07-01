de7db6d9085f415224142271d1a5c97c693d219c
Mukhamed <shisyr2106@gmail.com>
2018-07-01 18:23:25 +0600

    features-such-as-add-remove-getone-getlist-update-events

--- NestJS/src/Event/event.listener.ts
+++ NestJS/src/Event/event.listener.ts
@@ -4,14 +4,75 @@ import { Controller } from '@nestjs/common';
 @Controller('event')
 export class EventController {
   constructor(private readonly eventService: EventService) {
-
-     var exchange =  connection.declareExchange("hello", 'direct');
-     var queue = connection.declareQueue("event", {durable: true});
-     queue.bind(exchange, 'black');
-     queue.activateConsumer((message) => {
-         console.log("Message received: " + JSON.parse(message.getContent()));
-     }, {noAck: true});
+    var exchange =  connection.declareExchange("ExchangeName", 'direct');
+    var queue = connection.declareQueue("event", {durable: true});
+    queue.bind(exchange, 'black');
+    queue.activateConsumer(async (message) => {
+         let json = JSON.parse(message.getContent());
+         await run(json, this.eventService, function(err, response){
+           let msg = {
+             "title": json.title,
+             "body": {}
+           }
+           msg.body = response;
+           console.log("Message received: ");
+           console.log(msg);
+      });
+    }, {noAck: true});
   }
-
-
+}
+async function run(json, eventService, callback){
+  if(json.title == "create"){
+    await eventService.createEvent(json, function(err, response){
+      if(err){
+        callback(err, '');
+      }
+      else{
+        callback('', response);
+      }
+    });
+  }
+  else if(json.title == "update"){
+    await eventService.updateEvent(json, function(err, response){
+      if(err){
+        callback(err, '');
+      }
+      else{
+        callback('', response);
+      }
+    });
+  }
+  else if(json.title == "getlist"){
+    await eventService.getList(function(err, response){
+      if(err){
+        callback(err, '');
+      }
+      else{
+        callback('', response);
+      }
+    });
+  }
+  else if(json.title == "getone"){
+    await eventService.getOne(json, function(err, response){
+      if(err){
+        callback(err, '');
+      }
+      else{
+        callback('', response);
+      }
+    });
+  }
+  else if(json.title == "remove"){
+    await eventService.removeEvent(json, function(err, response){
+      if(err){
+        callback(err, '');
+      }
+      else{
+        callback('', response);
+      }
+    });
+  }
+  else{
+    callback("Send back message about incorrect title!", '');
+  }
 }
--- NestJS/src/Event/event.service.ts
+++ NestJS/src/Event/event.service.ts
@@ -11,13 +11,58 @@ export class EventService {
       private readonly eventRepository: Repository<Event>,
       ) {}
 
-  async createEvent(event: Event){
-    google.run(event, function(response) {
-      const eventOfDatabase = event.content;
-      eventOfDatabase.id_event = response.id;
-      console.log(response.id);
-      getRepository(Event).save(eventOfDatabase);
-    })
+  async createEvent(event: Event, callback){
+    google.run(event, function(err, response) {
+      if(err){
+        callback(err, '');
+      }
+      else{
+        const eventOfDatabase = event.body;
+        eventOfDatabase.id_event = response.id;
+        getRepository(Event).insert(eventOfDatabase);
+        callback('', eventOfDatabase);
+      }
+    });
   }
 
+  async getList(callback){
+    const events = await getRepository(Event).find({});
+    callback('', events);
+  }
+  async getOne(event: Event, callback) {
+    const eventOfDatabase = await getRepository(Event).findOne({id_event: event.body.id_event});
+    if(eventOfDatabase){
+      callback('', eventOfDatabase);
+    }
+    else{
+      callback(`cannot find 'event of database' because of incorrect 'id_event'`, '');
+    }
+}
+  async updateEvent(event: Event, callback){
+    const eventOfDatabase = await getRepository(Event).findOne({id_event: event.body.id_event});
+    event.body.id = eventOfDatabase.id;
+    event.body.id_event = eventOfDatabase.id_event;
+    google.run(event, function(err, response){
+      if(err){
+        callback(err, '');
+      }
+      else{
+        getRepository(Event).save(event.body);
+        callback('', event.body);
+      }
+    });
+  }
+  async removeEvent(event: Event, callback){
+    const eventOfDatabase = await getRepository(Event).findOne({id_event: event.body.id_event});
+    event.body = eventOfDatabase;
+    google.run(event, function(err, response){
+      if(err){
+        callback(err, '');
+      }
+      else{
+        getRepository(Event).delete(eventOfDatabase);
+        callback('', response);
+      }
+    });
+  }
 }
