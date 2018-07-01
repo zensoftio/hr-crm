c6f73d36bd864aa561ad19a42bd58937749cb821
erlanamanatov <erlanamanatov@gmail.com>
2018-07-01 10:21:54 +0600

    create ui-candidate-detail
    *basic Presenter, View created for CandidateDetail
    *created layout for detailed candidate info
    *added onClickListener to Adapter in CandidatesList to open new window
    with candidate's detailed info

--- Android/ZensoftHRCRM/app/src/main/java/com/erkprog/zensofthrcrm/MainActivity.java
+++ Android/ZensoftHRCRM/app/src/main/java/com/erkprog/zensofthrcrm/MainActivity.java
@@ -11,6 +11,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 import com.erkprog.zensofthrcrm.ui.candidates.candidatesList.CandidatesFragment;
 
