820176dd310d79d5cb0f150844072e1bda15bca4
Nazar <nazarchampion@gmail.com>
2018-07-02 12:03:02 +0600

    archive modified

--- src/scenes/head/Archive.js
+++ src/scenes/head/Archive.js
@@ -1,34 +1,48 @@
 import React from 'react';
 import Header from '../general/Header';
-
+import TableList from '../../components/ui/Table';
 const Archive = () => {
 	let today = new Date().getDate() + '.' + (new Date().getMonth() + 1) + '.' + new Date().getFullYear();
-	const archive = [
-		{
-			vacancy: 'Python',
-			created: '12.12.2012',
-			closed: today,
-			amount: 2
-		},
-		{
-			vacancy: 'JS',
-			created: '12.2.2013',
-			closed: today,
-			amount: 1
-		},
-		{
-			vacancy: 'Java',
-			created: '12.2.2003',
-			closed: today,
-			amount: 1
-		}
+	// const archive = [
+	// 	{
+	// 		vacancy: 'Python',
+	// 		created: '12.12.2012',
+	// 		closed: today,
+	// 		amount: 2
+	// 	},
+	// 	{
+	// 		vacancy: 'JS',
+	// 		created: '12.2.2013',
+	// 		closed: today,
+	// 		amount: 1
+	// 	},
+	// 	{
+	// 		vacancy: 'Java',
+	// 		created: '12.2.2003',
+	// 		closed: today,
+	// 		amount: 1
+	// 	}
+	// ];
+	const data = [
+		[
+			'Python', '12.12.2012', today, 2
+		],
+		[
+			'JS', '10.02.2015', today, 1
+		],
+		[
+			'iOS', '02.11.2016', today, 1
+		]
+	],
+	header = [
+		'#', 'Название', 'Дата создания', 'Дата закрытия', 'Количество'
 	];
 
 	
   return (
     <div>
       <Header title="Архив Позиций"/>
-			<table>
+			{/* <table>
 				<tbody className="table_head">
 				<tr>
 					<th>Название</th>
@@ -47,7 +61,8 @@ const Archive = () => {
 					)
 				})}
 				</tbody>
-			</table>
+			</table> */}
+			<TableList header={header} data={data} />
     </div>
   )
 }
