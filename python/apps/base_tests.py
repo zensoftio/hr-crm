68578b9ccdb2c982937befc752343a8874b1e64e
Emir <amanbekoff@gmail.com>
2018-07-02 14:25:46 +0600

    Add base test class * Add base_tests.py * Add __init__.py

---
+++ python/apps/base_tests.py
@@ -0,0 +1,18 @@
+class ListCreateTestMixin(object):
+    url = None
+    model = None
+    serializer = None
+    request_body = {}
+
+    def test_creation(self):
+        response = self.client.post(self.url, self.request_body)
+        self.assertEqual(201, response.status_code)
+        self.assertEqual(1, self.model.objects.count())
+
+    def test_get_list(self):
+        response = self.client.get(self.url)
+        queryset = self.model.objects.all()
+        serializer = self.serializer(queryset, many=True)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data, serializer.data)
