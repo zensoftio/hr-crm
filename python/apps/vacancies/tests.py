da020c21c64a4e2b861d63c98874af946f39efed
Almaz <mositosi97@gmail.com>
2018-07-03 13:49:07 +0600

    Make 'create' test for app vacancies

--- python/apps/vacancies/tests.py
+++ python/apps/vacancies/tests.py
@@ -1,15 +1,40 @@
 from rest_framework.test import APITestCase
-from django.utils import timezone
 
-from apps.base_tests import CreateTestMixin, ListTestMixin
-from apps.requests.models import Request
-from apps.departments.models import Position, Department
-from apps.users.models import User
+from apps.base_tests import ListTestMixin, VacancyCreateTestMixin
 from .models import Vacancy
 from .serializers import VacancyListSerializer, VacancyCreateUpdateSerializer
 
 
-class VacancyCreateListTestCase(ListTestMixin, APITestCase):
+class VacancyListTestCase(ListTestMixin, APITestCase):
     url = '/vacancies/'
     model = Vacancy
     serializer = VacancyListSerializer
+
+
+class VacancyCreateListTestCase(VacancyCreateTestMixin, APITestCase):
+    url = '/vacancies/'
+    model = Vacancy
+    serializer = VacancyCreateUpdateSerializer
+
+    fixtures = ['candidates.json', 'departments.json', 'requests.json', 'users.json', 'vacancies.json']
+
+    request_body = {
+        "title": "Interns required",
+        "request": 1,
+        "created_by": 1,
+        "city": "Bishkek",
+        "address": "Bishkek",
+        "work_conditions": "Nice kitchen",
+        "experience": "0",
+        "working_hours": "FT",
+        "employment_type": "FT",
+        "salary_min": 200.0,
+        "salary_max": 1000.0,
+        "created": "2018-05-21T10:35:18+0000",
+        "last_published": "2018-05-21T10:35:18+0000",
+        "status": 1,
+        "requirements": [
+            7,
+            8
+        ]
+    }
