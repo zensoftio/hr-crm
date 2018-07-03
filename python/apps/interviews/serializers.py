d90533e034e672a97d1080eabeb611242c8d65bd
Emir <amanbekoff@gmail.com>
2018-07-03 15:21:59 +0600

    Move AuxCandidateSerializer from candidate app to interview * TODO: Fix this circular import error

--- python/apps/interviews/serializers.py
+++ python/apps/interviews/serializers.py
@@ -1,17 +1,33 @@
 from rest_framework import serializers
 
+from apps.candidates.models import Candidate
 from apps.users.serializers import AuxUserSerializer
 from .models import Interview, Criteria
+
+
+class AuxCandidateSerializer(serializers.ModelSerializer):
+    """Candidate Serializer for Candidates List and Candidate Detail endpoint"""
+
+    class Meta:
+        model = Candidate
+        depth = 3
+        fields = ('id', 'first_name', 'last_name', 'position')
 
 
 class InterviewListSerializer(serializers.ModelSerializer):
     """Serializer for Interviews List Endpoint"""
     interviewers = AuxUserSerializer(many=True)
+    candidate = AuxCandidateSerializer()
 
     class Meta:
         model = Interview
         depth = 3
         fields = ('id', 'date', 'status', 'candidate', 'interviewers')
+
+
+class InterviewDetailSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Interview
 
 
 class AuxInterviewSerializer(serializers.ModelSerializer):
@@ -28,3 +44,4 @@ class CriteriaSerializer(serializers.ModelSerializer):
     class Meta:
         model = Criteria
         fields = ('id', 'name', 'department')
+
