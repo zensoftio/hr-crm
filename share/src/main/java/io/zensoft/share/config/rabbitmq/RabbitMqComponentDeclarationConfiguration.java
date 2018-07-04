20674eb9f10a583fcb0f8cca999f7ecbd82cb2de
PlusRT <temirlan.renatov@gmail.com>
2018-07-04 13:47:08 +0600

    Configured jackson message converter * monolith said that they are sending snake case json attributes * Created jackson object mapper * renamed bean name from messageConverter to jsonMessageConverter

--- share/src/main/java/io/zensoft/share/config/rabbitmq/RabbitMqComponentDeclarationConfiguration.java
+++ share/src/main/java/io/zensoft/share/config/rabbitmq/RabbitMqComponentDeclarationConfiguration.java
@@ -1,7 +1,11 @@
 package io.zensoft.share.config.rabbitmq;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,8 +15,6 @@ public class RabbitMqComponentDeclarationConfiguration {
     public static final String TOPIC_SHARE = "share";
     public static final String QUEUE_FACEBOOK_PUBLISH = "facebookPublish";
     public static final String QUEUE_FACEBOOK_GET_INFO = "facebookGetInfo";
-    public static final String QUEUE_TELEGRAM_PUBLISH = "telegramPublish";
-    public static final String QUEUE_TELEGRAM_GET_INFO = "telegramGetInfo";
     public static final String QUEUE_DIESEL_PUBLISH = "dieselPublish";
     public static final String QUEUE_DIESEL_GET_INFO = "dieselGetInfo";
     public static final String QUEUE_JOB_KG_PUBLISH = "jobKgPublish";
@@ -34,16 +36,6 @@ public class RabbitMqComponentDeclarationConfiguration {
     }
 
     @Bean()
-    public Queue telegramPublisherQueue() {
-        return new Queue(QUEUE_TELEGRAM_PUBLISH);
-    }
-
-    @Bean
-    public Queue telegramGetInfoQueue() {
-        return new Queue(QUEUE_TELEGRAM_GET_INFO);
-    }
-
-    @Bean()
     public Queue dieselPublisherQueue() {
         return new Queue(QUEUE_DIESEL_PUBLISH);
     }
@@ -62,5 +54,18 @@ public class RabbitMqComponentDeclarationConfiguration {
     public Queue jobKgGetInfoQueue() {
         return new Queue(QUEUE_JOB_KG_GET_INFO);
     }
+
+    @Bean
+    public ObjectMapper snakeCaseObjectMapper() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.setPropertyNamingStrategy(PropertyNamingStrategy.SNAKE_CASE);
+        return objectMapper;
+    }
+
+    @Bean
+    public MessageConverter jsonMessageConverter(ObjectMapper snakeCaseObjectMapper) {
+        return new Jackson2JsonMessageConverter(snakeCaseObjectMapper);
+    }
+
 
 }
