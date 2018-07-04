aeb62177581312fb46222b138fea6c20f46ccfdb
quanta <quantagonista@gmail.com>
2018-07-04 11:53:47 +0600

    Fix configs 2

--- python/config/base.py
+++ python/config/base.py
@@ -129,7 +129,7 @@ class Base(Configuration):
                                     'django_filters.rest_framework.DjangoFilterBackend',
                                     'rest_framework.filters.OrderingFilter',),
         'DEFAULT_AUTHENTICATION_CLASSES': (
-            'oauth2_provider.contrib.rest_framework.OAuth2Authentication',  # django-oauth-toolkit >= 1.0.0
+            'oauth2_provider.contrib.rest_framework.OAuth2Authentication',
             'rest_framework_social_oauth2.authentication.SocialAuthentication',
         ),
     }
