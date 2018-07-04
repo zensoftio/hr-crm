9ce671270cc9874525ca69a3da2f7c8c7e3d9cde
PlusRT <temirlan.renatov@gmail.com>
2018-07-04 10:37:26 +0600

    Deleted VacancyBuilder

--- share/src/main/java/io/zensoft/share/service/converter/DefaultVacancyConverterService.java
+++ share/src/main/java/io/zensoft/share/service/converter/DefaultVacancyConverterService.java
@@ -2,14 +2,13 @@ package io.zensoft.share.service.converter;
 
 import io.zensoft.share.dto.VacancyDto;
 import io.zensoft.share.model.Vacancy;
-import io.zensoft.share.model.builder.VacancyBuilder;
 import org.springframework.stereotype.Service;
 
 /**
  * Created by temirlan on 7/3/18.
  */
 @Service
-public class DefaultVacancyConverterService implements DtoConverterService<Vacancy,VacancyDto> {
+public class DefaultVacancyConverterService implements DtoConverterService<Vacancy, VacancyDto> {
 
     @Override
     public VacancyDto toDto(Vacancy vacancy) {
@@ -18,24 +17,6 @@ public class DefaultVacancyConverterService implements DtoConverterService<Vacan
 
     @Override
     public Vacancy fromDto(VacancyDto vacancyDto) {
-        VacancyBuilder vacancyBuilder = new VacancyBuilder();
-        vacancyBuilder.
-                setVacancyId(vacancyDto.getId()).
-                setTitle(vacancyDto.getTitle()).
-                setSphere(vacancyDto.getSphere()).
-                setDescription(vacancyDto.getDescription()).
-                setCompetencies(vacancyDto.getCompetencies()).
-                setRequirements(vacancyDto.getRequirements()).
-                setResponsibilities(vacancyDto.getResponsibilities()).
-                setWorkingConditions(vacancyDto.getWorkingConditions()).
-                setSalaryMin(vacancyDto.getSalaryMin()).
-                setSalaryMax(vacancyDto.getSalaryMax()).
-                setAdditionalInfo(vacancyDto.getAdditionalInfo()).
-                setLinks(vacancyDto.getLinks()).
-                setImageUrl(vacancyDto.getImageUrl()).
-                setEducation(vacancyDto.getEducation()).
-                setSchedule(vacancyDto.getSchedule()).
-                setCity(vacancyDto.getCity());
-        return vacancyBuilder.build();
+        return null;
     }
 }
