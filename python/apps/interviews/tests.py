33bc40573043a031e03e785eca140710aefb5fcc
Almaz <mositosi97@gmail.com>
2018-07-04 11:02:55 +0600

    Finish interviews endpoint test

--- python/apps/interviews/tests.py
+++ python/apps/interviews/tests.py
@@ -1,9 +1,11 @@
 from rest_framework.test import APITestCase
+from django.utils import timezone
 
 from apps.base_tests import ListTestMixin, CreateTestMixin, GetInstanceTestMixin
 from apps.departments.models import Department
 from .models import Criteria, Interview
-from .serializers import CriteriaSerializer, InterviewerSerializer, InterviewerDetailSerializer
+from apps.candidates.models import Candidate
+from .serializers import CriteriaSerializer, InterviewerSerializer, InterviewDetailSerializer
 
 
 class CriteriaCreateListTestCase(ListTestMixin, CreateTestMixin, APITestCase):
@@ -28,9 +30,13 @@ class InterviewListTestCase(ListTestMixin, APITestCase):
 class InterviewDetailTest(GetInstanceTestMixin, APITestCase):
     url = '/interviews/'
     model = Interview
-    serializer = InterviewerDetailSerializer
+    serializer = InterviewDetailSerializer
 
-    fixtures = ['candidates.json', 'departments.json', 'requests.json', 'users.json', 'interviews.json', 'vacancies.json']
+    fixtures = ['candidates.json', 'departments.json', 'requests.json', 'users.json', 'interviews.json',
+                'vacancies.json']
 
     def setUp(self):
-        self.instance = Interview.objects.get(pk=2)
+        self.instance = Interview.objects.create(
+            date=timezone.now(),
+            candidate=Candidate.objects.get(pk=1)
+        )
