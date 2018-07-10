79c4dc0370bfe6d6113bff8d78af0ad36daecff6
derkenbaev <derkenbaev.azamat@gmail.com>
2018-07-10 16:13:44 +0600

    add implementation of the VacancyRetriever interface

---
+++ share/src/main/java/io/zensoft/share/service/DefaultVacancyRetriever.java
@@ -0,0 +1,12 @@
+package io.zensoft.share.service;
+
+import io.zensoft.share.model.Vacancy;
+import io.zensoft.share.model.VacancyResponse;
+
+public class DefaultVacancyRetriever implements VacancyRetriever {
+
+    @Override
+    public VacancyResponse getInfo(Vacancy vacancy) {
+        return null;
+    }
+}
