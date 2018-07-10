f0e59e144d0d9bd12b72b53b40e6d6026e3fed5b
erlanamanatov <erlanamanatov@gmail.com>
2018-07-10 16:56:51 +0600

    Create ApiClient and ApiInterface

---
+++ Android/ZensoftHRCRM/app/src/main/java/com/erkprog/zensofthrcrm/data/network/remote/ApiInterface.java
@@ -0,0 +1,32 @@
+package com.erkprog.zensofthrcrm.data.network.remote;
+
+import com.erkprog.zensofthrcrm.data.entity.Candidate;
+import com.erkprog.zensofthrcrm.data.entity.CandidatesResponse;
+import com.erkprog.zensofthrcrm.data.entity.CriteriasResponse;
+import com.erkprog.zensofthrcrm.data.entity.InterviewsResponse;
+import com.erkprog.zensofthrcrm.data.entity.RequestsResponse;
+import com.erkprog.zensofthrcrm.data.entity.VacanciesResponse;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface ApiInterface {
+
+  @GET("")
+  Call<CandidatesResponse> getCandidates();
+
+  @GET("")
+  Call<CriteriasResponse> getCriteries();
+
+  @GET("")
+  Call<Candidate> getDetailedCandidate();
+
+  @GET("")
+  Call<InterviewsResponse> getInterviews();
+
+  @GET("")
+  Call<VacanciesResponse> getVacancies();
+
+  @GET("")
+  Call<RequestsResponse> getRequests();
+}
