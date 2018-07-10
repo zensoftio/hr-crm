1d9c554acf30f2092cc9ce51d73d042ad16d74c4
derkenbaev <derkenbaev.azamat@gmail.com>
2018-07-10 18:26:27 +0600

    remove @Service annotation from interfaces

--- share/src/main/java/io/zensoft/share/service/model/VacancyModelService.java
+++ share/src/main/java/io/zensoft/share/service/model/VacancyModelService.java
@@ -6,7 +6,6 @@ import org.springframework.stereotype.Service;
 /**
  * Created by temirlan on 6/29/18.
  */
-@Service
 public interface VacancyModelService extends ModelRepositoryService<Vacancy, Long> {
 
 }
