0abb39bfceb14338b2e277ddb9dbad5b1930fe20
derkenbaev <derkenbaev.azamat@gmail.com>
2018-07-11 13:44:03 +0600

    return @Repository annotations

--- share/src/main/java/io/zensoft/share/repository/VacancyRepository.java
+++ share/src/main/java/io/zensoft/share/repository/VacancyRepository.java
@@ -2,7 +2,9 @@ package io.zensoft.share.repository;
 
 import io.zensoft.share.model.Vacancy;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface VacancyRepository extends JpaRepository<Vacancy, Long> {
 
 }
--- share/src/main/java/io/zensoft/share/repository/VacancyResponseRepository.java
+++ share/src/main/java/io/zensoft/share/repository/VacancyResponseRepository.java
@@ -4,9 +4,11 @@ import io.zensoft.share.model.PublisherServiceType;
 import io.zensoft.share.model.Vacancy;
 import io.zensoft.share.model.VacancyResponse;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface VacancyResponseRepository extends JpaRepository<VacancyResponse, Long> {
 
     List<VacancyResponse> getAllByVacancy(Vacancy vacancy);
