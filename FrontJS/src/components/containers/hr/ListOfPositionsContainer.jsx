c5fe05c93ac496c6694fe6f0a184e20744932ed2
Emir <amanbekoff@gmail.com>
2018-07-11 14:32:39 +0600

    Update fetching method. * Make it use local package FetchAPI

--- FrontJS/src/components/containers/hr/ListOfPositionsContainer.jsx
+++ FrontJS/src/components/containers/hr/ListOfPositionsContainer.jsx
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import TableList from "../../ui/Table";
 import { Button, CircularProgress} from '@material-ui/core';
 import AddIcon from '@material-ui/icons/Add';
+import {FetchDataAPI} from "../../../services/FetchDataAPI";
 
 const header = ['№', 'ЗАГОЛОВОК', 'ДАТА', 'КОЛ-ВО','СТАТУС', 'СОЗДАТЬ'];
 
@@ -12,7 +13,7 @@ const button = [
 ];
 
 class ListOfPositions extends Component {
-    
+
     constructor(props){
         super(props)
         this.state = {
@@ -21,51 +22,45 @@ class ListOfPositions extends Component {
     }
 
     dateConvert = (date) => {
-        const newDate = new Date(date).toLocaleString('ru', {
+        return new Date(date).toLocaleString('ru', {
             year: 'numeric',
             month: 'long',
             day: 'numeric'
         });
-        return newDate;
-    }
+    };
 
     initStatus = (status) => {
-        if(status === 1){
+        if(status === 'NOT_REVIEWED'){
             return "Не рассмотрено"
-        } else if(status === 2){
+        } else if(status === 'APPROVED'){
             return "Утверждено"
-        } else if(status === 3){
-            return  "Отклонено"            
+        } else if(status === 'DECLINED'){
+            return  "Отклонено"
         } else {
             return null
         }
-    }
+    };
 
     componentDidMount() {
-        const URL_REQUESTS = "http://159.65.153.5/api/v1/requests/";
-   
-        fetch(URL_REQUESTS, {
-            method: "GET"
-        }).then(res => res.json())
-        .then(json => json.results.map(item => (
-          {
-            requests_id: item.id,
-            title: item.position.name,
-            created: item.created,
-            quantity: item.count,
-            status: item.status
-          })
+        const URL_REQUESTS = "http://159.65.153.5/api/v1/requests";
+
+        const fetched = FetchDataAPI(URL_REQUESTS);
+        fetched.then(response => response.results.map(item => (
+            {
+                request_id: item.id,
+                title: item.position.name,
+                created: item.created,
+                quantity: item.count,
+                status: item.status
+            })
         )).then(data => this.setState({
             data
-        })).catch(error => {
-            alert("You send wrong request", error);
-        })
-   
+        }))
     }
-    
+
     addPreloader = () => {
         return  <CircularProgress />
-    }
+    };
 
     render() {
 
@@ -73,15 +68,15 @@ class ListOfPositions extends Component {
 
         const array = data.map(item => {
             return [
-                item.title, 
-                this.dateConvert(item.created), 
+                item.title,
+                this.dateConvert(item.created),
                 item.quantity,
                 this.initStatus(item.status),
                 button]
-        })
-        
-        return ( 
-            <TableList header={header} data={array}/> 
+        });
+
+        return (
+            <TableList header={header} data={array}/>
         )
     }
 }
