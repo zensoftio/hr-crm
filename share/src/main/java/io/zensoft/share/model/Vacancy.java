59d0a0edfcb5c2de9bb43f3a8a05d4f1b469cb5c
PlusRT <temirlan.renatov@gmail.com>
2018-07-11 11:12:11 +0600

    Edited dto standards

--- share/src/main/java/io/zensoft/share/model/Vacancy.java
+++ share/src/main/java/io/zensoft/share/model/Vacancy.java
@@ -15,22 +15,23 @@ public class Vacancy {
     @Id
     @GeneratedValue
     private Long id;
-    private int uid;
+    private String uuid;
     private String title;
     @OneToMany(mappedBy = "vacancy")
     private List<Requirement> requirements;
     private String city;
     private String address;
     private String position;
-    private int count;
+    private Integer count;
     @ElementCollection
-    @CollectionTable(name = "vacancy_working_condition", joinColumns = @JoinColumn(name = "vacancy_id"))
+    @CollectionTable(name = "vacancy_work_condition", joinColumns = @JoinColumn(name = "vacancy_id"))
     @Column(name = "name")
-    private List<String> workingConditions;
-    private String experience;
-    private String workingHours;
-    private String employmentType;
-    private int salaryMin;
-    private int salaryMax;
+    private List<String> workConditions;
+    @Enumerated(value = EnumType.STRING)
+    private WorkingHours workingHours;
+    private Integer salaryMin;
+    private Integer salaryMax;
     private String image;
+    private String responsibilities;
+    private String comment;
 }
