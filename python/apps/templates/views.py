bd5c06cdb95b81712d0faaa950d0283b5afa9037
erik.duisheev <erik.duysheev@mail.ru>
2018-07-12 15:28:19 +0600

    remove not-used code

--- python/apps/templates/views.py
+++ python/apps/templates/views.py
@@ -1,18 +1,16 @@
-from rest_framework import generics, status
+import json
+
+from rest_framework import generics
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from apps.templates.models import Template, Attachment
-from apps.templates.serializers import TemplateListSerializer, TemplateDetailSerializer, AttachmentSerializer
+from apps.templates.models import Attachment
+from apps.templates.serializers import AttachmentSerializer
 from apps.utils.rabbitmq import RabbitMQ
 from config.base import Base
-import json
 
 
 class TemplateListCreateView(APIView):
-    # queryset = Template.objects.all()
-    # serializer_class = TemplateListSerializer
-    # filter_fields = ('type',)
 
     def get(self, request, *args, **kwargs):
         data = {'title': 'FIND_ALL'}
@@ -24,11 +22,11 @@ class TemplateListCreateView(APIView):
                              queue_to_send='template', routing_key_to_send='template',
                              queue_to_receive='template3',
                              message=data)
-        r = rabbitmq_sender.response.decode('utf-8')
-        r = json.loads(r)
-        return Response(r)
+        response_data = rabbitmq_sender.response.decode('utf-8')
+        response_data = json.loads(response_data)
+        return Response(response_data)
 
-    def create(self, request, *args, **kwags):
+    def post(self, request, *args, **kwags):
         data = request.data
         data['title'] = 'CREATE'
         data = json.dumps(data)
@@ -39,33 +37,18 @@ class TemplateListCreateView(APIView):
                              queue_to_send='template', routing_key_to_send='template',
                              queue_to_receive='template3',
                              message=data)
-        r = rabbitmq_sender.response.decode('utf-8')
-        r = json.loads(r)
-        return Response(r)
-
-        # write_serializer = TemplateCreateSerializer(data=request.data)
-        # write_serializer.is_valid(raise_exception=True)
-        # self.perform_create(write_serializer)
-        # read_serializer = TemplateDetailSerializer(write_serializer.instance)
-        # return Response(read_serializer.data, status=status.HTTP_201_CREATED)
+        response_data = rabbitmq_sender.response.decode('utf-8')
+        response_data = json.loads(response_data)
+        return Response(response_data)
 
 
 class TemplateDetailView(APIView):
-    # queryset = Template.objects.all()
-    # serializer_class = TemplateDetailSerializer
 
-    def get(self, request, pk, format=None, *args, **kwargs):
+    def get(self, request, pk, *args, **kwargs):
         data = {
             "title": "FIND_ONE",
             "id": pk
         }
-        # data = {
-        #     "title": "UPDATE",
-        #     "id": 3,
-        #     "subject": "Erik",
-        #     "body": "Erik",
-        #     "attachment": "[a,b,c,d]"
-        # }
         data = json.dumps(data)
         print(type(data), data)
 
@@ -74,50 +57,42 @@ class TemplateDetailView(APIView):
                              queue_to_send='template', routing_key_to_send='template',
                              queue_to_receive='template3',
                              message=data)
-        r = rabbitmq_sender.response.decode('utf-8')
-        r = json.loads(r)
-        return Response(r)
+        response_data = rabbitmq_sender.response.decode('utf-8')
+        response_data = json.loads(response_data)
+        return Response(response_data)
 
-    def update(self, request, pk, format=None, *args, **kwargs):
+    def put(self, request, pk, *args, **kwargs):
         data = request.data
         data['title'] = 'UPDATE'
         data['id'] = pk
         data = json.dumps(data)
         print(type(data), data)
 
-        data = {
-            "task": "UPDATE",
-            "id": 2,
-            "subject": "Erik",
-            "body": "Erik",
-            "attachment": "Erik"
-        }
-        data = json.dumps(data)
-
         rabbitmq_sender = RabbitMQ(user=Base.RABBITMQ_USERNAME, password=Base.RABBITMQ_PASSWORD)
         rabbitmq_sender.call(exchange_name='exchangeForTemplate', exchange_type='direct',
                              queue_to_send='template', routing_key_to_send='template',
                              queue_to_receive='template3',
                              message=data)
-        r = rabbitmq_sender.response.decode('utf-8')
-        r = json.loads(r)
-        return Response(r)
+        response_data = rabbitmq_sender.response.decode('utf-8')
+        response_data = json.loads(response_data)
+        return Response(response_data)
 
-    def delete(self, request, pk, format=None, *args, **kwargs):
+    def delete(self, request, pk, *args, **kwargs):
         data = {
             "title": "DELETE",
             "id": pk
         }
         data = json.dumps(data)
         print(type(data), data)
+
         rabbitmq_sender = RabbitMQ(user=Base.RABBITMQ_USERNAME, password=Base.RABBITMQ_PASSWORD)
         rabbitmq_sender.call(exchange_name='exchangeForTemplate', exchange_type='direct',
                              queue_to_send='template', routing_key_to_send='template',
                              queue_to_receive='template3',
                              message=data)
-        r = rabbitmq_sender.response.decode('utf-8')
-        r = json.loads(r)
-        return Response(r)
+        response_data = rabbitmq_sender.response.decode('utf-8')
+        response_data = json.loads(response_data)
+        return Response(response_data)
 
 
 class AttachmentListCreateView(generics.ListCreateAPIView):
