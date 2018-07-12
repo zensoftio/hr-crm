dc0750341f4158f70f3c5d468ebea20a02d27e4e
akaev.jumgal <akaev.jumgal@gmail.com>
2018-07-12 13:45:51 +0600

    Update CreateVacancy.js

--- FrontJS/src/scenes/hr/CreateVacancy.js
+++ FrontJS/src/scenes/hr/CreateVacancy.js
@@ -1,16 +1,29 @@
 import React, {Component} from 'react';
+import { withStyles } from '@material-ui/core/styles';
 import CreateVacancyContainer from '../../components/containers/hr/CreateVacancyContainer';
 import Header from '../general/Header';
+import { Paper } from '@material-ui/core';
+
+const styles = () => ({
+    paperBox: {
+        margin: '1.5em 1em',
+        padding: '1em 1.5em'
+    }
+})
 
 class CreateVacancy extends Component {
     render() {
+        const { classes } = this.props;
+
         return (
           <div>
             <Header title="Создать Вакансию" />
-              <CreateVacancyContainer vacancyId={this.props.match.params.id}/>
+              <Paper className={classes.paperBox}>
+                <CreateVacancyContainer vacancyId={this.props.match.params.id}/>
+              </Paper>
           </div>
         );
     }
 }
 
-export default CreateVacancy;
+export default withStyles(styles) (CreateVacancy);
