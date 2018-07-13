252ac5a6f4c5eff11c2dafa06760ff7a8d66aa7b
derkenbaev <derkenbaev.azamat@gmail.com>
2018-07-13 10:44:34 +0600

    Use FacebookUser class to get page access token in FacebookPublisherService

--- share/src/main/java/io/zensoft/share/service/facebook/FacebookUser.java
+++ share/src/main/java/io/zensoft/share/service/facebook/FacebookUser.java
@@ -3,12 +3,14 @@ package io.zensoft.share.service.facebook;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
+@Service
 public class FacebookUser {
     private String accessToken;
     private String pageAccessToken;
