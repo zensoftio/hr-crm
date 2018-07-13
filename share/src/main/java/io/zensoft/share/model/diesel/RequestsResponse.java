3fd8635dccaea387afecb92c6a0b2e6b839d1f80
aidar-aibekov <aibekov-aidar@mail.ru>
2018-07-13 16:19:40 +0600

    add RequestResponse models

---
+++ share/src/main/java/io/zensoft/share/model/diesel/RequestsResponse.java
@@ -0,0 +1,16 @@
+package io.zensoft.share.model.diesel;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class RequestsResponse {
+    @Enumerated(value = EnumType.STRING)
+    private RequestStatus status;
+}
