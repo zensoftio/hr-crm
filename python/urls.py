d11eecf01b928cec219130146bfe4ad27d9f130c
quanta <quantagonista@gmail.com>
2018-07-16 15:54:30 +0600

    Create Token with Role - for frontend - role in header - refactor urls - move some urls into auth app - clean settings

--- python/urls.py
+++ python/urls.py
@@ -15,8 +15,7 @@ v1 = ([
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/v1/', include(v1)),
     path('api-auth/', include('rest_framework.urls')),
     path('auth/', include('rest_framework_social_oauth2.urls')),
-    path('api/v1/', include(v1)),
 ]
-
