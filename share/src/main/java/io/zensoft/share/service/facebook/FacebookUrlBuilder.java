054b1b55ef2fe073635170e99d6c5d0cad4eefe9
derkenbaev <derkenbaev.azamat@gmail.com>
2018-07-16 13:18:03 +0600

    add logs to FacebookPageUrlBuilder

--- share/src/main/java/io/zensoft/share/service/facebook/FacebookUrlBuilder.java
+++ share/src/main/java/io/zensoft/share/service/facebook/FacebookUrlBuilder.java
@@ -1,10 +1,12 @@
 package io.zensoft.share.service.facebook;
 
 import io.zensoft.share.model.Vacancy;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 public class FacebookUrlBuilder {
     private FacebookConfigs facebookConfigs;
 
@@ -14,6 +16,7 @@ public class FacebookUrlBuilder {
     }
 
     public String getPublishPhotoRequestUrl(Vacancy vacancy, String pageAccessToken) {
+        log.info("build url for publish photo request to Facebook Graph Api");
         String url = facebookConfigs.getPublishPhotoRequestUrlTemplate();
         url = url.replace("{pageId}", facebookConfigs.getPageId());
         url = url.replace("{photoUrl}", vacancy.getImage());
@@ -24,6 +27,7 @@ public class FacebookUrlBuilder {
     }
 
     public String getPublishTextRequestUrl(Vacancy vacancy, String pageAccessToken) {
+        log.info("build url for post request to Facebook Graph Api");
         String url = facebookConfigs.getPublishTextRequestUrlTemplate();
         url = url.replace("{pageId}", facebookConfigs.getPageId());
         url = url.replace("{message}", vacancy.getTitle());
