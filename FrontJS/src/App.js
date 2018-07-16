f0d45cd15bd4403981af5c15a7ee5906786a9c68
Nazar <nazarchampion@gmail.com>
2018-07-16 15:07:10 +0600

    approve decline pm

--- FrontJS/src/App.js
+++ FrontJS/src/App.js
@@ -30,7 +30,7 @@ export default class App extends Component {
 
     render() {
         if(this.state.session) {
-            return <User userRole="head" />
+            return <User userRole="pm" />
         }
         return(
             <div>                
--- FrontJS/src/scenes/pm/EditRequest.js
+++ FrontJS/src/scenes/pm/EditRequest.js
@@ -5,98 +5,125 @@ import MultipleSelect from '../../components/ui/MultipleSelect';
 import PlusMinus from '../../components/ui/PlusMinus';
 import Button from '@material-ui/core/Button'
 import { TextField } from '@material-ui/core';
+import { REQUESTS_URL } from '../../utils/urls';
+import { FetchDataAPI } from '../../services/FetchDataAPI';
+import { PutDataAPI } from '../../services/PutDataAPI';
 
-const EditRequest = () => {
+class EditRequest extends React.Component {
 
-	const	department = [
-		'PM Department', 
-		'Human Resources',
-		'Head of Department'
-	],
-	developer = [
-		'Python Dev',
-		'JS Dev',
-		'Java Dev'
-	],
-	requirements = [
-		'one',
-		'two',
-		'three',
-		'five',
-		'four'
-	],
-	additional_requirements = [
-		'lorem',
-		'ipsum',
-		'dolores',
-		'katanta'
-	]
+	constructor(props){
+		super(props)
+		this.state = {
+			department :'',
+			count : 0,
+			position : '',
+			requirements : [],
+			url: REQUESTS_URL + '/' + this.props.match.params.id
+		}
+	}
 
-	const instyle = {
-		display: 'inline-block',
-		marginRight: 20
-	};
-	const divstyle = {
-		margin: 20
-	};
+	componentDidMount() {
+		FetchDataAPI( this.state.url )
+			.then(response => this.setState(
+				{
+					department : response.position.department.name,
+					count : response.count,
+					position : response.position.name,
+					requirements : response.requirements.map(res => res.name)
+				}
+			))			
+	}
 
-	return(
-		<div>
-			<Header title="Запросы"/>
+	getCountFromChild = (dataFromChild) => {
+		this.setState({
+			count: dataFromChild
+		});
+	}
 
-			<div style={divstyle}>
-				<label>
-					<span style={instyle}>ОТДЕЛ:</span>							
-					<Select optionValue={department}/>							
-				</label>						
-			</div>
+	handleApprove = () => {
+		const { url, count} = this.state;
+		PutDataAPI( url, {
+			"count": count,
+			"status": "APPROVED"
+		});
+	}
 
-			<div style={divstyle}>
-				<label>
-					<span style={instyle}>КОЛИЧЕСТВО:</span>
-					<PlusMinus />							
-				</label>
-			</div>
+	handleDecline = () => {
+		const { url, count} = this.state;
+		PutDataAPI( url, {
+			"status": "DECLINED",
+			"count": count,
+		});
+	}
 
-			<div style={divstyle}>
-				<label>
-					<span style={instyle}>НАЗВАНИЕ:</span>	
-					<Select optionValue={developer}/>								
-				</label>						
-			</div>
-
-			<div style={divstyle}>
-				<label>
-					<span style={instyle}>ТРЕБОВАНИЯ:</span>	
-					<span style={instyle}>
-						<MultipleSelect optionValue={requirements}/>
-					</span>	
-				</label>						
-			</div>
-
-			<div style={divstyle}>
-				<label>
-					<span style={instyle}>ДОПОЛНИТЕЛЬНЫЕ ТРЕБОВАНИЯ:</span>	
-					<span style={instyle}>
-						<MultipleSelect optionValue={additional_requirements}/>
-					</span>	
-				</label>						
-			</div>
-
-			<div style={divstyle}>
-				<label>
-					<span style={instyle}>ОБЩИЕ ТРЕБОВАНИЯ:</span>
-					<TextField multiline placeholder="введите текст" />
-				</label>
-			</div>
-
-			<div style={divstyle}>
-				<span style={instyle}><Button variant="contained" color="primary">Утвердить</Button></span>
-				<span style={instyle}><Button variant="contained" color="secondary">Отклонить</Button></span>
-			</div>
+	render() {			
 			
-		</div>
-	);
+			const { department, position, count, requirements } = this.state;
+
+			return(
+				<div>
+					<Header title="Запросы"/>
+						<div className="div_items">
+							<label>
+								<span className="labels">ОТДЕЛ:</span>							
+								<select value={department}>
+									<option>{department}</option>
+								</select>								
+							</label>						
+						</div>
+			
+						<div className="div_items">
+							<label>
+								<span className="labels">КОЛИЧЕСТВО:</span>
+								<PlusMinus getCountFromChild={this.getCountFromChild}  countStarts={count}/>						
+							</label>
+						</div>
+			
+						<div className="div_items">
+							<label>
+								<span className="labels">НАЗВАНИЕ:</span>	
+								<select value={position}>
+									<option>{position}</option>
+								</select>								
+							</label>						
+						</div>
+			
+						<div className="div_items">
+							<label>
+								<span className="labels">ТРЕБОВАНИЯ:</span>	
+								<span className="labels">
+									<MultipleSelect optionValue={requirements}/>
+								</span>	
+							</label>						
+						</div>
+					
+						<div className="div_items">
+							<label>
+								<span className="labels">ОБЩИЕ ТРЕБОВАНИЯ:</span>
+								<TextField multiline placeholder="введите текст" />
+							</label>
+						</div>
+			
+						<div className="div_items">
+							<span className="labels">
+								<Button 
+									onClick={this.handleApprove}
+									variant="contained" 
+									color="primary">Утвердить
+								</Button>
+							</span>
+							<span className="labels">
+								<Button 
+									onClick={this.handleDecline}
+									variant="contained" color="secondary">Отклонить
+								</Button>
+							</span>
+						</div>
+					
+				</div>
+			);
+
+	}
 }
 
 export default EditRequest;
