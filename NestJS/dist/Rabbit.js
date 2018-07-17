cbbf1d04771d7d7df4e312a4011ff18b368ca27a
Mukhamed <shisyr2106@gmail.com>
2018-07-17 15:59:33 +0600

    NestJS deleted production of files

--- NestJS/dist/Rabbit.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const Amqp = require("amqp-ts");
-const connection = new Amqp.Connection("amqp://guest:guest@159.65.153.5:5672");
-exports.default = connection;
-//# sourceMappingURL=Rabbit.js.map
