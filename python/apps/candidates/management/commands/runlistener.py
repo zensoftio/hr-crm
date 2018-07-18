1767dccd69a3557e20057a8424171449c259cf44
quanta <quantagonista@gmail.com>
2018-07-18 09:59:00 +0600

    Alter Connections for RabbitMQ

--- python/apps/candidates/management/commands/runlistener.py
+++ python/apps/candidates/management/commands/runlistener.py
@@ -1,4 +1,5 @@
 from django.core.management import BaseCommand
+from pika.exceptions import ConnectionClosed
 
 from apps.candidates.listener import main
 
@@ -7,4 +8,7 @@ class Command(BaseCommand):
     help = 'Runs Listener for RabbitMQ queue which send new Candidates instances to create'
 
     def handle(self, *args, **options):
-        main()
+        try:
+            main()
+        except ConnectionClosed:
+            self.handle(*args, **options)
