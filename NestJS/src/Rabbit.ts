b757ca74e704c866285b86fb61101624eee9a2a4
Emir <amanbekoff@gmail.com>
2018-07-18 14:01:35 +0600

    Update rabbitmq host

--- NestJS/src/Rabbit.ts
+++ NestJS/src/Rabbit.ts
@@ -1,6 +1,6 @@
 
 import * as Amqp from "amqp-ts";
 
-const connection = new Amqp.Connection("amqp://guest:guest@159.65.153.5:5672");
+const connection = new Amqp.Connection("amqp://guest:guest@rabbitmq:5672");
 
 export default connection;
